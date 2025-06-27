d71996cb43d2e69cb7f224d4bf8fb6f0d966769f
Vinicius <viniciusr.camargo@gmail.com>
2025-06-27 15:04:43 -0300

    Códigos do curso TECHDAY para o Senai Araçatuba

---
+++ vetor.py
@@ -0,0 +1,9 @@
+#declaração de um array ou vetor ou lista ou ainda coleção de dados
+carros = ["Fusca", "Chevette", "Kombi", "Monza", "Santana"]
+
+print(carros[4], carros[0])
+
+for carro in carros:
+    print(carro)
+
+
---
+++ while.py
@@ -0,0 +1,5 @@
+livros = ["1984", "Sapiens", "Mindset", "Laranja Mecânica", "O Hobbit"]
+
+while len(livros) > 0:
+    livro = livros.pop(0)
+    print(f"Lendo o livro {livro}")
